c71c7cf8d778c4b7a37791bfc1cbd68e362a0344
abdoutony <abdouaallb@gmail.com>
2023-05-01 20:35:19 +0100

    finishing auth part

--- app/database/index.js
+++ app/database/index.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const configurations = require("../config/app")
 const exampleSeed = require("./data/example.json");
+const userSeed = require("./data/user.json");
 const {Example} = require("../models/example")
+const User = require("../models/user")
 mongoose.set("strictQuery", false);
 mongoose
   .connect(configurations.database.dev, {
@@ -13,7 +15,8 @@ mongoose
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
     );
-    await Example.insertMany(exampleSeed);
+    // await Example.insertMany(exampleSeed);
+    await User.insertMany(userSeed);
     mongoose.connection.close(function () {
       console.log("** Disconnected from database **");
       process.exit(0);
--- app/routes/auth.js
+++ app/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const AuthController = require('../controllers/AuthController');
 const { verifyToken } = require('../middleware/auth');
-
+const {isAdmin} = require("../middleware/auth")
 module.exports= ()=>{
         router.post('/register', AuthController.register);
         router.post('/login', AuthController.login);
